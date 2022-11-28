7190f997224d5722943438f23eec240fd57bfc12
Mehe_od76@itstep.edu.az <Mehe_od76@itstep.edu.az>
2022-11-28 14:20:12 +0400

    Add project files.

---
+++ FlyWeight/Program.cs
@@ -0,0 +1,98 @@
+﻿interface Shape
+{
+    void draw();
+}
+
+public class Circle : Shape // implement
+{
+    private string color;
+    private int x;
+    private int y;
+    private int radius;
+
+    public Circle(String color)
+    {
+        this.color = color;
+    }
+
+    public void setX(int x)
+    {
+        this.x = x;
+    }
+
+    public void setY(int y)
+    {
+        this.y = y;
+    }
+
+    public void setRadius(int radius)
+    {
+        this.radius = radius;
+    }
+
+    
+   public void draw() // override
+    {
+        Console.WriteLine("Circle: Draw() [Color : " + color + ", x : " + x + ", y :" + y + ", radius :" + radius);
+    }
+}
+
+//public class ShapeFactory
+//{
+
+ 
+//    private static HashMap circleMap = new HashMap();
+
+//    public static Shape getCircle(string color)
+//    {
+//        Circle circle = (Circle)circleMap.get(color);
+
+//        if (circle == null)
+//        {
+//            circle = new Circle(color);
+//            circleMap.put(color, circle);
+//            Console.WriteLine("Creating circle of color : " + color);
+//        }
+//        return circle;
+//    }
+//}
+
+
+//class Program
+//{
+//    private static string colors[] = { "Red", "Green", "Blue", "White", "Black" };
+//   public static void main(String[] args)
+//    {
+
+//        for (int i = 0; i < 20; ++i)
+//        {
+//            Circle circle = (Circle)ShapeFactory.getCircle(getRandomColor());
+//            circle.setX(getRandomX());
+//            circle.setY(getRandomY());
+//            circle.setRadius(100);
+//            circle.draw();
+//        }
+//    }
+//    private static string getRandomColor()
+//    {
+//        return colors[(int)(Math.random() * colors.length)];
+//    }
+//    private static int getRandomX()
+//    {
+//        return (int)(Math.random() * 100);
+//    }
+//    private static int getRandomY()
+//    {
+//        return (int)(Math.random() * 100);
+//    }
+//}
+
+
+
+//class Program { 
+//    public static void Main()
+//    {
+//        Shape shape = new Circle();
+//        Console.WriteLine("");
+//    }
+//}
---
+++ HomeworkDp/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace Proxy;
+
+interface Image
+{
+    void display();
+}
+
+
+public class RealImage : Image // implement Image
+{
+
+    private string fileName;
+
+    public RealImage(string fileName)
+    {
+        this.fileName = fileName;
+        loadFromDisk(fileName);
+    }
+
+
+    public void display()
+    {
+        Console.WriteLine("Displaying " + fileName); // Override
+    }
+
+    private void loadFromDisk(string fileName)
+    {
+        Console.WriteLine("Loading " + fileName);
+    }
+}
+
+public class ProxyImage : Image{
+
+   private RealImage realImage;
+   private string fileName;
+
+   public ProxyImage(string fileName){
+      this.fileName = fileName;
+   }
+
+   
+   public void display() {
+      if(realImage == null){
+         realImage = new RealImage(fileName); // Override
+        }
+      realImage.display();
+   }
+}
+
+class Program
+{
+
+    public static void Main()
+    {
+        Image image = new ProxyImage("test_10mb.jpg");
+
+        //image will be loaded from disk
+        image.display();
+        Console.WriteLine("");
+
+        //image will not be loaded from disk
+        image.display();
+    }
+}
